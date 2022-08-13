31afe70758799b45750e144c1002de3dd47136d7
assamess hymn book <110216271+AssameseHymnBook@users.noreply.github.com>
2022-08-13 20:40:26 +0530

    DELET

--- index.js.js
+++
@@ -1,7 +0,0 @@
-var yourName = prompt("What is your name ?");
-var yourNamefirstchar = yourName.slice(0,1);
-var uppercaseyourNamefirstchar = yourNamefirstchar.toUpperCase();
-var restofyourName = yourName.slice(1,name.lenght);
-var capitalisedyourName = uppercaseyourNamefirstchar + restofyourName;
-alert("Hello," + " " + capitalisedyourName +" " + "😃" +
-     "May God Bless You.");
